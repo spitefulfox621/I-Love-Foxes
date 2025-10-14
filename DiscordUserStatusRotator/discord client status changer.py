967e1752d9fec32ab51d7aa23be3f8ccf5cb34bd
Ayyvery <125614099+Avery-FurFox@users.noreply.github.com>
2025-10-14 03:18:02 +0200

    DUSR upload
    very simple script for rotating statuses on your discord client (not for bot users)
    only dependencies are toml (for the config), aiohttp (for the web requests) and colorama (for colorful log messages)
    
    configuration goes as follows:
    run it once and let it generate the .toml config file, then close it
    in the `token` field, input your client's token (here's how you can find it: https://www.reddit.com/r/discordapp/comments/1h7isk5/comment/m0m9qwn)
    sleep_interval is the number of seconds between each status, dont set this too low as it can get you rate limited
    api_url is the api endpoint, dont change this unless you know what you're doing
    statuses is a list of statuses following the format: 
    "[status] <emoji> message" 
    where
        status is either online, idle, dnd, invisible or offline
        emoji is an optional discord emoji (both animated and regular ones supported)
        message is the actual status message itself, must be within discords character limit else it will throw an error
    once you're happy with the statuses, save the config and restart the program, and it should say [200 OK], if it doesnt you may have to check if the token is correct or the statuses follow the format, if all else fails report the issue here
    to close the program simply press ctrl+c

---
+++ DiscordUserStatusRotator/discord client status changer.py
@@ -0,0 +1,94 @@
+import asyncio, re, toml, aiohttp, colorama, itertools, traceback
+colorama.init(autoreset=True)
+status_pattern = re.compile(r'^\[(?P<status>\w+)\]\s*(?:(?P<emoji>(?:<a?:\w+:\d+>))\s*)?(?P<message>.+)$')
+discord_pattern = pattern = re.compile(r'^<(?P<animated>a?):(?P<name>\w+):(?P<id>\d+)>$')
+
+try:
+    config = toml.load(open("dcsc_config.toml", "r", encoding="utf-8"))
+except:
+    config = {
+        "token": "YOUR_TOKEN_HERE",
+        "sleep_interval": 3600,
+        "api_url": "https://discord.com/api/v10/users/@me/settings",
+        "statuses": [
+            "[dnd] example status",
+            "[idle] <:wave:123456789123456789> status with a custom emoji",
+            "[invisible] <a:waving:123456789123456789> status with a custom animated emoji",
+            ]
+        }
+finally:
+    toml.dump(config, open("dcsc_config.toml", "w", encoding="utf-8"))
+
+async def change_status(session:aiohttp.ClientSession, status_inf:dict={"status": str, "message": str}, emoji:dict={"name":str, "id":str, "animated":bool}):
+    status = str(status_inf.get("status"))
+    text = str(status_inf.get("message"))
+    emoji_name = emoji.get("name")
+    emoji_id = emoji.get("id")
+    emoji_animated = emoji.get("animated")
+
+    header = {"Authorization": config["token"]}
+    json = {
+        "status": status,
+        "custom_status": {
+            "text": text,
+            "emoji_name": emoji_name if emoji_name is not None else None,
+            "emoji_id": emoji_id if emoji_id is not None else None,
+            "emoji_animated": emoji_animated if emoji_animated is not None else False,
+            }
+        }
+    try:
+        resp = await session.patch(url=config["api_url"], headers=header, json=json)
+        color = (colorama.Fore.GREEN) if resp.status == 200 else (colorama.Fore.YELLOW)
+        print(color + f"[{resp.status} {resp.reason}]")
+        if resp.status != 200:
+            data = await resp.json()
+            try:
+                err_msg, err_issue = data.get("message"), data.get("errors")
+                print(color + f"    {err_msg} {err_issue}")
+            except:
+                pass
+    except Exception as e:
+        print(colorama.Fore.RED + f"FUCK\n{e}")
+
+def process_status(text):
+    _status = _emoji_name = _emoji_id = _emoji_animated = _message = None
+    match = status_pattern.match(text)
+    if match:
+        m = match.groupdict()
+        _status = m.get("status")
+        _maybe_emoji = m.get("emoji")
+        _message = m.get("message")
+        if _maybe_emoji:
+            discord_match = discord_pattern.match(_maybe_emoji)
+            if discord_match:
+                d = discord_match.groupdict()
+                _emoji_animated = d.get("animated")
+                _emoji_name = d.get("name")
+                _emoji_id = d.get("id")
+
+
+    return _status, _emoji_name, _emoji_id, _emoji_animated, _message
+
+async def main():
+    print(colorama.Fore.BLUE + "status changer running, press ctr+c to exit")
+    session = aiohttp.ClientSession()
+    statuses = itertools.cycle(config["statuses"])
+    while True:
+        status, emoji_name, emoji_id, emoji_animated, message = process_status(next(statuses))
+        if status:
+            await change_status(session=session, status_inf={"status": status, "message": message}, emoji={"name": emoji_name, "id": emoji_id, "animated": emoji_animated})
+            await asyncio.sleep(config["sleep_interval"])
+        else:
+            raise Warning("status didn't match pattern")
+
+def get_traceback():
+    return traceback.format_exc()
+
+try:
+    asyncio.run(main())
+except Warning as w:
+    print(colorama.Fore.YELLOW + f"warning: {get_traceback()}")
+except Exception as e:
+    print(colorama.Fore.RED + f"exception: {get_traceback()}")
+except BaseException as b:
+    print(colorama.Fore.RED + colorama.Style.BRIGHT + f"error: {get_traceback()}")
