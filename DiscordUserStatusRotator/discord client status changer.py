4767ed76147705cff232be1bfe6e0a91d69c8b74
Ayyvery <125614099+spitefulfox621@users.noreply.github.com>
2025-10-18 00:44:22 +0200

    DUSR v1.1 upload
    cleaned up code a little and switched to json for the config file
    config now shows the version
    config now shows available cycle modes
    rotator now allows for different status-cycle modes, "cycle" goes through each item in the list in order, "random" picks a random item every time
    configured options get printed on startup
    script name, script version and script author get printed on startup
    in addition to the status-code, the selected status will also get printed every cycle, allowing you to cross-reference it with your client to see if everything is working correctly (for example if the script prints "[200 OK] ([dnd] hello world)" your client's status should also have switched to [dnd] hello world

--- DiscordUserStatusRotator/discord client status changer.py
+++ DiscordUserStatusRotator/discord client status changer.py
@@ -1,15 +1,19 @@
-import asyncio, re, toml, aiohttp, colorama, itertools, traceback
+import asyncio, re, json, aiohttp, colorama, itertools, traceback, random
 colorama.init(autoreset=True)
 status_pattern = re.compile(r'^\[(?P<status>\w+)\]\s*(?:(?P<emoji>(?:<a?:\w+:\d+>))\s*)?(?P<message>.+)$')
 discord_pattern = pattern = re.compile(r'^<(?P<animated>a?):(?P<name>\w+):(?P<id>\d+)>$')
 
+######## config section ########################################################################
 try:
-    config = toml.load(open("dcsc_config.toml", "r", encoding="utf-8"))
+    config = json.load(open("dcsc_config.json", "r", encoding="utf-8"))
 except:
     config = {
+        "_modes": ["cycle", "random"],
+        "_version": 1.1,
         "token": "YOUR_TOKEN_HERE",
         "sleep_interval": 3600,
         "api_url": "https://discord.com/api/v10/users/@me/settings",
+        "mode": "cycle",
         "statuses": [
             "[dnd] example status",
             "[idle] <:wave:123456789123456789> status with a custom emoji",
@@ -17,17 +21,18 @@ except:
             ]
         }
 finally:
-    toml.dump(config, open("dcsc_config.toml", "w", encoding="utf-8"))
+    json.dump(config, open("dcsc_config.json", "w", encoding="utf-8"), indent=2)
+######## config section ########################################################################
 
+######## main code section #################################################################################################################################
 async def change_status(session:aiohttp.ClientSession, status_inf:dict={"status": str, "message": str}, emoji:dict={"name":str, "id":str, "animated":bool}):
     status = str(status_inf.get("status"))
     text = str(status_inf.get("message"))
     emoji_name = emoji.get("name")
     emoji_id = emoji.get("id")
     emoji_animated = emoji.get("animated")
-
     header = {"Authorization": config["token"]}
-    json = {
+    jsondata = {
         "status": status,
         "custom_status": {
             "text": text,
@@ -37,20 +42,44 @@ async def change_status(session:aiohttp.ClientSession, status_inf:dict={"status"
             }
         }
     try:
-        resp = await session.patch(url=config["api_url"], headers=header, json=json)
+        resp = await session.patch(url=config["api_url"], headers=header, json=jsondata)
         color = (colorama.Fore.GREEN) if resp.status == 200 else (colorama.Fore.YELLOW)
-        print(color + f"[{resp.status} {resp.reason}]")
+        print(color + f"[{resp.status} {resp.reason}] ([{status}] {text}) next status in {config["sleep_interval"]} seconds")
         if resp.status != 200:
             data = await resp.json()
             try:
                 err_msg, err_issue = data.get("message"), data.get("errors")
                 print(color + f"    {err_msg} {err_issue}")
             except:
-                pass
+                raise
     except Exception as e:
         print(colorama.Fore.RED + f"FUCK\n{e}")
 
+async def updater_loop():
+    print(colorama.Fore.BLUE + f"status changer {colorama.Fore.GREEN}v{config["_version"]}{colorama.Fore.BLUE} by {colorama.Fore.MAGENTA}Ayyvery", "\n", "press ctr+c to exit")
+    print(colorama.Fore.CYAN + f"""config:
+    token: {'*' * len(config["token"])}
+    cycle mode: {config["mode"]}
+    cycle interval: {config["sleep_interval"]}
+    """)
+    session = aiohttp.ClientSession()
+    if config["mode"] == "cycle":
+        statuses = itertools.cycle(config["statuses"])
+        statusgetter = lambda: next(statuses)
+    elif config["mode"] == "random":
+        statusgetter = lambda: random.choice(config["statuses"])
+    while True:
+        status, emoji_name, emoji_id, emoji_animated, message = process_status(statusgetter())
+        if status:
+            await change_status(session=session, status_inf={"status": status, "message": message}, emoji={"name": emoji_name, "id": emoji_id, "animated": emoji_animated})
+            await asyncio.sleep(config["sleep_interval"])
+        else:
+            raise Warning("status didn't match pattern")
+######## main code section #################################################################################################################################
+
+######## helper code section #############################################
 def process_status(text):
+    """Processes a status line"""
     _status = _emoji_name = _emoji_id = _emoji_animated = _message = None
     match = status_pattern.match(text)
     if match:
@@ -65,30 +94,18 @@ def process_status(text):
                 _emoji_animated = d.get("animated")
                 _emoji_name = d.get("name")
                 _emoji_id = d.get("id")
-
-
     return _status, _emoji_name, _emoji_id, _emoji_animated, _message
-
-async def main():
-    print(colorama.Fore.BLUE + "status changer running, press ctr+c to exit")
-    session = aiohttp.ClientSession()
-    statuses = itertools.cycle(config["statuses"])
-    while True:
-        status, emoji_name, emoji_id, emoji_animated, message = process_status(next(statuses))
-        if status:
-            await change_status(session=session, status_inf={"status": status, "message": message}, emoji={"name": emoji_name, "id": emoji_id, "animated": emoji_animated})
-            await asyncio.sleep(config["sleep_interval"])
-        else:
-            raise Warning("status didn't match pattern")
-
-def get_traceback():
-    return traceback.format_exc()
+######## helper code section #############################################
 
 try:
-    asyncio.run(main())
+    asyncio.run(updater_loop())
+except KeyboardInterrupt:
+    print(colorama.Fore.BLUE + "exiting...")
+    exit()
 except Warning as w:
-    print(colorama.Fore.YELLOW + f"warning: {get_traceback()}")
+    print(colorama.Fore.YELLOW + f"warning: {traceback.format_exc()}")
 except Exception as e:
-    print(colorama.Fore.RED + f"exception: {get_traceback()}")
+    print(colorama.Fore.RED + f"exception: {traceback.format_exc()}")
 except BaseException as b:
-    print(colorama.Fore.RED + colorama.Style.BRIGHT + f"error: {get_traceback()}")
+    print(colorama.Fore.RED + colorama.Style.BRIGHT + f"error: {traceback.format_exc()}")
+    exit()
